0e5302bc8540500e3086c1084cf4b9eaca1b7fff
Walaahmed <wlaa61462@gmail.com>
2025-06-10 07:35:00 +0200

    حفظ تعديلات App.jsx قبل الدمج

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import PageNotFound from './components/PageNotFound';
 import './App.css'
 import Register from './components/Register'
 import Login from './components/Login'
-=======
->>>>>>> 791fefdbd3d9099758385297b1fe1e31e157b0c7
 
 import './App.css'
 // import AboutSection from'./components/AboutPage.jsx'
 // import ProductDetails from'./components/ProductDetails.jsx'
 function App() {
-<<<<<<< HEAD
   useState(0)
 
   return (
@@ -20,19 +16,11 @@ function App() {
     <Register/>
    </div>
    </>
-=======
  
 
-  return (
-    <>
-      {/* <AboutSection/>
-      <br></br>
-       <br></br>
-       <br></br>
-      <ProductDetails/> */}
-    </>
->>>>>>> 791fefdbd3d9099758385297b1fe1e31e157b0c7
-  )
+      )    
+      
+
 }
 
 export default App
