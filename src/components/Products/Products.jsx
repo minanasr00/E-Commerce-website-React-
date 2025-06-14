3a1d9c92184ee90a5cad22da44e1d07c24a479f7
minanasr00 <62456457+minanasr00@users.noreply.github.com>
2025-06-14 06:22:38 +0300

    sa

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -99,7 +99,7 @@ const { toggleWishlist, isInWishlist ,deleteFromWish } = useWishlist();
   };
 
   return (
-    <div className="bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] min-h-screen p-6">
+    <div className="bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] min-h-screen p-6 pt-20">
       <h2 className="text-3xl font-bold text-center mb-8">Our Products</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
