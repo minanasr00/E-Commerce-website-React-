12da9d0429786c734250a6f2e98f2aec9a6865fc
minanasr00 <62456457+minanasr00@users.noreply.github.com>
2025-06-15 12:48:07 +0300

    ,

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,12 @@
 import React from "react";
  import svg from "../../assets/404.svg"; 
  import './PageNot.module.css'
- const PageNotFound = () => {
-     return ( <> <div className="h-screen cont-404">
+import { useNavigate } from "react-router-dom";
+const PageNotFound = () => {
+     const navigate = useNavigate()
+     return ( <> <div className="h-screen cont-404 pt-12">
          <img src={svg} className="h-full w-full object-contain" alt="svg" /> 
-         <button className="absolute top-4 right-4 bg-gray-300 text-black p-2 rounded font-[beatrice] cursor-pointer hover:bg-black hover:text-white">Back to Home</button> 
+         <button onClick={()=>{navigate("/")}} className="absolute mt-24 top-4 right-4 bg-gray-300 text-black p-2 rounded font-[beatrice] cursor-pointer hover:bg-black hover:text-white">Back to Home</button> 
          </div> </> );
           };
            export default PageNotFound;
